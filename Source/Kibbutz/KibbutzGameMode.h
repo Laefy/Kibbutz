fcc83cbee8e7f801748e470bbbed5936c49552c9
Celine NOEL <laefy22@gmail.com>
2016-12-09 16:55:19 +0100

    Initial commit. Creation of the UE4 project.

---
+++ Source/Kibbutz/KibbutzGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "KibbutzGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KIBBUTZ_API AKibbutzGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
