8a682160203544a3d567f0719e5420ad5b32cc95
Olivier Daire <daire.olivier@gmail.com>
2017-03-26 18:53:59 +0200

    Make quest checkAdvancement() callable from blueprint

--- Source/Kibbutz/Quest/Quest.h
+++ Source/Kibbutz/Quest/Quest.h
@@ -21,6 +21,7 @@ public:
 	virtual void BeginPlay() override;
 
 	// Call this in order to check whether to advance in the quest.
+	UFUNCTION(BlueprintCallable, Category = "Quest")
 	virtual bool CheckAdvancement();
 
 	// Called to check whether the current step has been completed or not.
